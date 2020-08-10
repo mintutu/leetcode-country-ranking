0ede4ae8e68d0ae7dcfc65a267d009b5b6c2a939
Lam Nguyen <nguyen.tunglam@shopee.com>
2020-08-10 18:16:02 +0800

    install scout monitoring

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
+import os
 from app import app
+from scout_apm.flask import ScoutApm
+
+ScoutApm(app)
+
+# Scout settings
+app.config["SCOUT_MONITOR"] = os.getenv("SCOUT_MONITOR", "false")
+app.config["SCOUT_KEY"] = os.getenv("SCOUT_KEY", "")
+app.config["SCOUT_NAME"] = "leetcode-country-ranking"
 
 if __name__ == '__main__':
     app.run()
